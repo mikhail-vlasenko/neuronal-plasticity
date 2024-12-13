cbaf20fcc79904584c77d0e89fef436d851af7c5
Mikhail <mikhail.v.vlasenko@gmail.com>
2024-12-13 23:18:04 +0100

    linear inhibitory homeostasis, worse than exponential

--- src/equations/inhibitory_homeostasis.py
+++ src/equations/inhibitory_homeostasis.py
@@ -4,20 +4,37 @@ amplitude = 2
 inhib_s_initial = -1
 baseline_inhib_s = inhib_s_initial
 
+use_linear_model = False
+linear_change = 0.05
+linear_min = -3
+
 
 inhibitory_synapse_model = '''
 s : 1
 '''
 
+# on pre, we should decrease inhibition (make it larger, less negative),
+# as that will increase the amount of times on post is called, and achieve e-i balance
 inhibitory_on_pre = '''
 s = clip(s, -10, 0)
 ge_post += s
 s += exp(-(s - baseline_inhib_s)/amplitude)*learning_rate
 '''
+if use_linear_model:
+    inhibitory_on_pre = '''
+        s = clip(s, linear_min, 0)
+        ge_post += s
+        s += linear_change
+    '''
 
+# on post, we should increase inhibition, as it means the excitatory (post) neuron is firing too much
 inhibitory_on_post = '''
 s -= exp((s - baseline_inhib_s)/amplitude)*learning_rate/exc_to_inhib_ratio
 '''
+if use_linear_model:
+    inhibitory_on_post = '''
+        s -= linear_change
+    '''
 
 INHIBITORY_SYNAPSE_PARAMS = {
     'model': inhibitory_synapse_model,
